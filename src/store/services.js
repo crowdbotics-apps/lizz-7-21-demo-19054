902b429e11b1c2ade0b34f6565cf64a02e0b3aab
Crowdbotics <team@crowdbotics.com>
2020-07-21 21:51:42 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,9 +1,20 @@
 import axios from "axios"
+import {} from "react-native-dotenv"
 const dogAPI = axios.create({
   baseURL: "https://dog.ceo/api/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+const freeDogAPI = axios.create({
+  baseURL: "https://dog.ceo/api",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function dogapi_get_breeds_image_random_read() {
   return dogAPI.get(`/breeds/image/random`)
 }
-export const apiService = { dogapi_get_breeds_image_random_read }
+function freedogapi_get_breeds_image_random_read() {
+  return freeDogAPI.get(`/breeds/image/random`)
+}
+export const apiService = {
+  dogapi_get_breeds_image_random_read,
+  freedogapi_get_breeds_image_random_read
+}
